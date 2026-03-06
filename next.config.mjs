de8e1f0f43d9dc5c67ab0a233ec45967ff3ee09b
User <fernanda_madrid@unitec.edu>
2026-03-05 18:04:12 -0600

    Actualiza lockfile tras instalar emailjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // PARA NETLIFY: descomenta la siguiente linea antes de hacer deploy
-  // output: 'export',
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
